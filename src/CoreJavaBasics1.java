cab22c6eae24fba07e2d60991c207dae7d966a1a
Rajat Sharan <errajatsharan@gmail.com>
2022-07-02 21:25:39 +0530

    DataType

--- src/CoreJavaBasics1.java
+++ src/CoreJavaBasics1.java
@@ -4,7 +4,14 @@ public class CoreJavaBasics1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int mynumb=5;
+		String Test = "Rajat";
+		char Letter='d';
+		double d =1.2;
+		boolean check=true;
 		
+		System.out.println(mynumb);
+		System.out.println(Test + "THis is the test string");
 		
 
 	}
