3da4a2888faa08d5ffd9c6e8510fbb19550966e1
Rajat Sharan <errajatsharan@gmail.com>
2022-07-02 21:34:34 +0530

    Array List

---
+++ src/Array_Java.java
@@ -0,0 +1,20 @@
+
+public class Array_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 //Arrays
+		int[] arr= new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=4;
+		arr[3]=5;
+		arr[4]=6;
+		
+		int [] array2= {1,2,3,4,5,6};
+		System.out.println(arr[0]);
+		System.out.println(array2[4]);
+	}
+
+}
