0a9baed9c3a39f5b952ae63220a4a947ff94dd1a
Rajat Sharan <errajatsharan@gmail.com>
2022-07-03 02:36:06 +0530

    Locators

---
+++ src/selenium/Locators.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","D:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("Rajat");
+		driver.findElement(By.name("inputPassword")).sendKeys("Rajat");
+		driver.findElement(By.className("signInBtn")).click();
+		
+		
+		
+
+	}
+
+}
