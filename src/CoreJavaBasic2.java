c0d6be885df4bf361ea8b0d3f28db7304ca1169d
Rajat Sharan <errajatsharan@gmail.com>
2022-07-02 22:43:56 +0530

    Check if array is multiple of 2

---
+++ src/CoreJavaBasic2.java
@@ -0,0 +1,28 @@
+
+public class CoreJavaBasic2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] array2= {1,2,3,4,5,6,8,9,10,100,200};
+		
+		//Check if array is multiple of 2
+		
+		for(int i=0; i<array2.length;i++) 
+		
+		{
+			if(array2[i]%2==0) 
+			{
+				System.out.println(array2[i]);
+			}
+			else
+			{
+				System.out.println(array2[i] + " Is not multiple of two " );
+			}
+			
+		}
+		
+		
+	}
+
+}
