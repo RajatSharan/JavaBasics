32dacad9b38301e272da654fa42ec8e1459197c6
Rajat Sharan <errajatsharan@gmail.com>
2022-07-03 11:54:18 +0530

    Implicit wait

--- src/selenium/Locators.java
+++ src/selenium/Locators.java
@@ -1,5 +1,7 @@
 package selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,14 +11,17 @@ public class Locators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	   //Implicit Wait		
+	   //Introducing Class name and Css Selector locators to identify elements
 		
 		System.setProperty("webdriver.chrome.driver","D:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("Rajat");
 		driver.findElement(By.name("inputPassword")).sendKeys("Rajat");
 		driver.findElement(By.className("signInBtn")).click();
-		
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		
 		
 
